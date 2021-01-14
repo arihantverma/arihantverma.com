70a388aaf7086c475b3df8f1ab057970ad3235c6
Arihant Verma <arihant.verma@go-mmt.com>
2021-01-15 00:43:57 +0530

    enable css min nunjucks filter

--- .eleventy.js
+++ .eleventy.js
@@ -167,9 +167,9 @@ module.exports = function (eleventyConfig) {
   });
 
   // inline css https://www.11ty.dev/docs/quicktips/inline-css/
-  // eleventyConfig.addFilter("cssmin", function(code) {
-  //   return new CleanCSS({}).minify(code).styles;
-  // });
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = new Set();
