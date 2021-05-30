e7f0833cab75c2feee5cd97e302340a76b90e8b5
arihantverma <arihantverma1994@gmail.com>
2021-05-30 19:55:57 +0530

    new post, new post, design changes

--- .eleventy.js
+++ .eleventy.js
@@ -218,7 +218,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("images");
-  // eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy({ "favicon-data": "/" });
 
