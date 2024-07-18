b55ccf1921bc6926d6725eba8789683078828822
Arihant Verma <arihantverma1994@gmail.com>
2024-07-18 20:01:52 +0530

    revamp init

--- _data/envdata.js
+++
@@ -1,3 +0,0 @@
-export default {
-  environment: process.env.ELEVENTY_ENV ?? 'development',
-}
--- js/utils/get-readable-date.js
+++
@@ -1,9 +0,0 @@
-import { DateTime } from "luxon";
-import { readableDateFormatForLuxon } from './constants.js';
-
-export default function getReadableDate(dateObj) {
-  return DateTime.fromJSDate(dateObj, { zone: "utc+05:30" }).toFormat(
-    readableDateFormatForLuxon
-  );
-}
-
